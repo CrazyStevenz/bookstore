5fbace459b5a0730c0ba40983590133db840444e
CrazyStevenz <spapanatsios@gmail.com>
2020-05-29 19:33:03 +0300

    Add Product model id and database annotations

--- app/src/main/java/com/crazystevenz/bookstore/model/Product.java
+++ app/src/main/java/com/crazystevenz/bookstore/model/Product.java
@@ -1,6 +1,12 @@
 package com.crazystevenz.bookstore.model;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "product")
 public class Product {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
     private String name;
     private int amount;
     private double price;
@@ -9,6 +15,14 @@ public class Product {
         this.name = name;
         this.amount = amount;
         this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
