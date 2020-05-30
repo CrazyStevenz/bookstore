07da960797ca5d66c9cf15d3b8eb1a55faeaf13f
CrazyStevenz <spapanatsios@gmail.com>
2020-05-30 16:45:22 +0300

    Change home to store

---
+++ app/src/main/java/com/crazystevenz/bookstore/ui/store/StoreViewModel.java
@@ -0,0 +1,19 @@
+package com.crazystevenz.bookstore.ui.store;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class StoreViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public StoreViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
--- app/src/main/java/com/crazystevenz/bookstore/view/MainActivity.java
+++ app/src/main/java/com/crazystevenz/bookstore/view/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_userlist)
+                R.id.nav_store, R.id.nav_gallery, R.id.nav_userlist)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
