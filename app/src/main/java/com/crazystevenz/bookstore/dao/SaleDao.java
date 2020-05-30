cf61f0d6e16ae7ee65e0b41c441c09c499ddf29b
CrazyStevenz <spapanatsios@gmail.com>
2020-05-30 23:06:38 +0300

    Add SaleDao

---
+++ app/src/main/java/com/crazystevenz/bookstore/dao/SaleDao.java
@@ -0,0 +1,29 @@
+package com.crazystevenz.bookstore.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.crazystevenz.bookstore.model.Customer;
+import com.crazystevenz.bookstore.model.Sale;
+
+import java.util.List;
+
+public interface SaleDao {
+    @Query("SELECT * FROM sale WHERE id = :id")
+    Customer get(int id);
+
+    @Query("SELECT * FROM sale")
+    LiveData<List<Sale>> getAll();
+
+    @Insert
+    void insert(Sale sale);
+
+    @Update
+    void update(Sale sale);
+
+    @Delete
+    void delete(Sale sale);
+}
