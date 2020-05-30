5ca9dc63e9b51dd547d4c7001b1cb370611506ff
CrazyStevenz <spapanatsios@gmail.com>
2020-05-30 20:51:22 +0300

    Remove options menu

--- app/src/main/java/com/crazystevenz/bookstore/view/MainActivity.java
+++ app/src/main/java/com/crazystevenz/bookstore/view/MainActivity.java
@@ -49,13 +49,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
