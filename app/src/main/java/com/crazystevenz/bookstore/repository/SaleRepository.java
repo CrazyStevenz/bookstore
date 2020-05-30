d4859d9ff4a0fc902096099b9f82ebe75a4fc679
CrazyStevenz <spapanatsios@gmail.com>
2020-05-30 23:07:28 +0300

    Add SaleRepository

---
+++ app/src/main/java/com/crazystevenz/bookstore/repository/SaleRepository.java
@@ -0,0 +1,26 @@
+package com.crazystevenz.bookstore.repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.crazystevenz.bookstore.dao.SaleDao;
+import com.crazystevenz.bookstore.model.Sale;
+
+import java.util.List;
+
+public class SaleRepository {
+
+    private SaleDao mSaleDao;
+    private LiveData<List<Sale>> mSales;
+
+    public SaleRepository(Application application) {
+        AppDatabase db = AppDatabase.getInstance(application);
+        mSaleDao = db.saleDao();
+        mSales = mSaleDao.getAll();
+    }
+
+    public LiveData<List<Sale>> getAll() {
+        return mSales;
+    }
+}
