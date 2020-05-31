d41bd960bb00417b0f276122f27117a5de694af9
CrazyStevenz <spapanatsios@gmail.com>
2020-05-31 23:30:32 +0300

    Revert "Remove unused dao functions"
    This reverts commit 8082b6b2

--- app/src/main/java/com/crazystevenz/bookstore/dao/CustomerDao.java
+++ app/src/main/java/com/crazystevenz/bookstore/dao/CustomerDao.java
@@ -25,6 +25,15 @@ public interface CustomerDao {
     @Insert
     void insert(Customer customer);
 
+    @Insert
+    void insertAll(Customer... customers);
+
     @Update
     void update(Customer customer);
+
+    @Delete
+    void delete(Customer customer);
+
+    @Query("DELETE FROM customer")
+    void deleteAll();
 }
--- app/src/main/java/com/crazystevenz/bookstore/dao/ProductDao.java
+++ app/src/main/java/com/crazystevenz/bookstore/dao/ProductDao.java
@@ -2,6 +2,7 @@ package com.crazystevenz.bookstore.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -19,12 +20,24 @@ public interface ProductDao {
     @Query("SELECT * FROM product")
     LiveData<List<Product>> getAll();
 
+    @Query("SELECT * FROM product WHERE name LIKE :name LIMIT 1")
+    Product getByName(String name);
+
     @Query("UPDATE product SET amount = amount + 1 WHERE id = :id")
     void incrementAmount(int id);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Product product);
 
+    @Insert
+    void insertAll(Product... products);
+
     @Update
     void update(Product product);
+
+    @Delete
+    void delete(Product product);
+
+    @Query("DELETE FROM product")
+    void deleteAll();
 }
