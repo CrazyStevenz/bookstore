18a299eb0dddacf02961450ad1ef00eaa24f5584
CrazyStevenz <spapanatsios@gmail.com>
2020-05-31 05:14:44 +0300

    Rework Cart classes

--- app/src/main/java/com/crazystevenz/bookstore/view/cart/CartFragment.java
+++ app/src/main/java/com/crazystevenz/bookstore/view/cart/CartFragment.java
@@ -1,5 +1,6 @@
 package com.crazystevenz.bookstore.view.cart;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,6 @@ import com.crazystevenz.bookstore.R;
 import com.crazystevenz.bookstore.model.Customer;
 import com.crazystevenz.bookstore.model.Product;
 import com.crazystevenz.bookstore.model.Sale;
-import com.crazystevenz.bookstore.viewmodel.SaleViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,29 +25,31 @@ import java.util.concurrent.ExecutionException;
 
 public class CartFragment extends Fragment {
 
-    private SaleViewModel saleViewModel;
+    private CartViewModel cartViewModel;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        final View root = inflater.inflate(R.layout.fragment_cart, container, false);
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-        saleViewModel = new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication()).create(SaleViewModel.class);
-        saleViewModel.getIncompleteByUserId(Customer.getUserInstance().getId()).observe(getViewLifecycleOwner(), new Observer<List<Sale>>() {
+        Application app = (getActivity().getApplication());
+
+        cartViewModel = new ViewModelProvider.AndroidViewModelFactory(app).create(CartViewModel.class);
+        cartViewModel.getIncompleteByUserId(Customer.getUserInstance().getId()).observe(getViewLifecycleOwner(), new Observer<List<Sale>>() {
             @Override
             public void onChanged(List<Sale> sales) {
-                recyclerView = root.findViewById(R.id.recyclerview_product_list);
+                recyclerView = getView().findViewById(R.id.recyclerview_product_list);
 
-                layoutManager = new LinearLayoutManager(root.getContext());
+                layoutManager = new LinearLayoutManager(getContext());
                 recyclerView.setLayoutManager(layoutManager);
 
                 List<Product> products = new ArrayList<>();
                 for (Sale sale : sales) {
 
                     try {
-                        Product product = saleViewModel.getProductById(sale.getProductId());
+                        Product product = cartViewModel.getProductById(sale.getProductId());
                         products.add(product);
                     } catch (ExecutionException | InterruptedException e) {
                         Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -58,7 +60,10 @@ public class CartFragment extends Fragment {
                 recyclerView.setAdapter(mAdapter);
             }
         });
+    }
 
-        return root;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cart, container, false);
     }
 }
--- app/src/main/java/com/crazystevenz/bookstore/view/cart/CartViewModel.java
+++ app/src/main/java/com/crazystevenz/bookstore/view/cart/CartViewModel.java
@@ -1,19 +1,41 @@
 package com.crazystevenz.bookstore.view.cart;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class CartViewModel extends ViewModel {
+import com.crazystevenz.bookstore.model.Product;
+import com.crazystevenz.bookstore.model.Sale;
+import com.crazystevenz.bookstore.repository.ProductRepository;
+import com.crazystevenz.bookstore.repository.SaleRepository;
 
-    private MutableLiveData<String> mText;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
-    public CartViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+public class CartViewModel extends AndroidViewModel {
+
+    private SaleRepository saleRepository;
+    private ProductRepository productRepository;
+    private LiveData<List<Sale>> sales;
+
+    public CartViewModel(@NonNull Application application) {
+        super(application);
+        saleRepository = new SaleRepository(application);
+        productRepository = new ProductRepository(application);
+        sales = saleRepository.getAll();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<Sale>> getAll() {
+        return sales;
+    }
+
+    public LiveData<List<Sale>> getIncompleteByUserId(int id) {
+        return saleRepository.getIncompleteByUserId(id);
+    }
+
+    public Product getProductById(int id) throws ExecutionException, InterruptedException {
+        return productRepository.getProductById(id);
     }
 }
